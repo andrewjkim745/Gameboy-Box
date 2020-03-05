72112cae1b15e0bc96a3b5f853cefccffa70d891
Andrew J Kim <andrewk745@gmail.com>
2020-03-05 03:03:38 -0500

    made nav and backdrop

--- gameboy-box/src/App.js
+++ gameboy-box/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Landing from './Components/Landing'
+import NavBar from './Components/NavBar'
 
 function App() {
   return (
-
-    
-  );
+    <>
+    <NavBar/>
+    <Landing/>
+    </>
+  )
 }
 
 export default App;
---
+++ gameboy-box/src/Components/Landing.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Styles/Landing.scss'
+
+
+
+class Landing extends React.Component {
+    constructor(props) {
+        super(props) 
+
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <div className='backdrop-container'>
+            <div className='backdrop'></div>
+            </div>
+        )
+    }
+}
+
+
+export default Landing 
