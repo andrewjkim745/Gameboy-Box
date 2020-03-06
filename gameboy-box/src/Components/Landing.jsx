23e2e96495a369a41004f2098407b475a3246f67
Andrew J Kim <andrewk745@gmail.com>
2020-03-06 03:40:46 -0500

    finished first half of landing page

--- gameboy-box/src/Components/Landing.jsx
+++ gameboy-box/src/Components/Landing.jsx
@@ -13,11 +13,22 @@ class Landing extends React.Component {
     }
 
 
+
+
     render() {
         return (
+            <>
             <div className='backdrop-container'>
             <div className='backdrop'></div>
             </div>
+            <div className='landing-quote'>
+                <h2>Track Games you've played</h2>
+                <br></br>
+                <h2>Save games you want to play</h2>
+                <br></br>
+                <h2>Tell your friends what's good.</h2>
+            </div>
+            </>
         )
     }
 }
