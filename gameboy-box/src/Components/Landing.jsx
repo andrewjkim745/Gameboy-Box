759c6d6253cfae5b2fde1a4f498bff6552a7fe85
Andrew J Kim <andrewk745@gmail.com>
2020-03-09 00:52:22 -0400

    made 2nd half part of first half of landing page

--- gameboy-box/src/Components/Landing.jsx
+++ gameboy-box/src/Components/Landing.jsx
@@ -5,7 +5,7 @@ import './Styles/Landing.scss'
 
 class Landing extends React.Component {
     constructor(props) {
-        super(props) 
+        super(props)
 
         this.state = {
 
@@ -18,16 +18,23 @@ class Landing extends React.Component {
     render() {
         return (
             <>
-            <div className='backdrop-container'>
-            <div className='backdrop'></div>
-            </div>
-            <div className='landing-quote'>
-                <h2>Track Games you've played</h2>
-                <br></br>
-                <h2>Save games you want to play</h2>
-                <br></br>
-                <h2>Tell your friends what's good.</h2>
-            </div>
+                <div className='backdrop-container'>
+                    <div className='backdrop'></div>
+                </div>
+                <div className='landing-quote'>
+                    <h2>Track Games you've played</h2>
+                    <br></br>
+                    <h2>Save games you want to play</h2>
+                    <br></br>
+                    <h2>Tell your friends what's good.</h2>
+                    <a>GET STARTED - IT'S FREE</a>
+                    <div className='platforms'>
+                        <p className='social'>The social network for classic GBA lovers. Also available on </p>
+                        <img src='https://i.imgur.com/6eO3w1V.png'></img>
+                        <img src='https://i.imgur.com/lVE3fWI.png'></img>
+                    </div>
+                    
+                </div>
             </>
         )
     }
