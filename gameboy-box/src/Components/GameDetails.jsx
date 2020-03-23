0a1c3e30fbd94c967b837d52187cfe7b28b5b201
Andrew J Kim <andrewk745@gmail.com>
2020-03-23 07:21:29 -0400

    finished gamedetails component

--- gameboy-box/src/Components/GameDetails.jsx
+++ gameboy-box/src/Components/GameDetails.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 
 
 
@@ -9,9 +8,18 @@ const GameDetails = (props) => {
             <div className='firstColumn'>
                 <img src={props.src}></img>
                 <div>
-                    
+                    <img src='https://i.imgur.com/rXfPua4.png'></img>
+                    <p>{props.views}</p>
+                    <img src='https://i.imgur.com/7eYqP1x.png'></img>
+                    <p>{props.lists}</p>
+                    <img src='https://i.imgur.com/1aiORiI.png'></img>
+                    <p>{props.favorites}</p>
+                    <img src='https://i.imgur.com/j9Gr6GR.png'></img>
+                    <p>{props.ranking[0]}</p>
                 </div>
             </div>
         </div>
     )
 }
+
+export default GameDetails
