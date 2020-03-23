7423213f17f72ed492405b7f22e5b2c88af616e3
Andrew J Kim <andrewk745@gmail.com>
2020-03-23 10:24:54 -0400

    added gamedetails routes

--- gameboy-box/src/Components/Routes/index.js
+++ gameboy-box/src/Components/Routes/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Landing from '../Landing'
+import GameDetails from '../GameDetails'
 
 
 
-const Routes = () => {
+const Routes = (props) => {
     return (
         <Switch>
             <Route exact path='/' component={Landing}/>
+            <Route exact path='/games/:id' render={props => <GameDetails {...props}/>}/>
         </Switch>
     )
 }
