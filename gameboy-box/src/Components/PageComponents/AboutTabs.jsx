1793d59fd1b8d535a257b7e1deb830c2fb54f8da
Andrew J Kim <andrewk745@gmail.com>
2020-03-25 17:50:55 -0400

    finished about component

--- gameboy-box/src/Components/PageComponents/AboutTabs.jsx
+++ gameboy-box/src/Components/PageComponents/AboutTabs.jsx
@@ -1,28 +1,35 @@
 import React from 'react'
+import '../Styles/AboutTabs.scss'
 
 
 
-
-export default class AboutTabs extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            clicked: false
-        }
-    }
-
-    onClick = () => {
-        this.setState({
-            clicked: true
-        })
-    }
-
-    render() {
-        return (
-            <div onClick={this.onClick} className={this.state.clicked ? 'selected' : 'notSelected'}>
-                <h3 className='tabTitle'>{props.title}</h3>
+const AboutTabs = (props) => {
+    return (
+        <div className='tabsContainer'>
+            <div className={props.frequentClass}>
+                <h3 onClick={props.frequentClick}>Frequent questions</h3>
             </div>
-        )
-    }
+            <div className={props.proClass}>
+                <h3 onClick={props.membershipClick}>Pro Membership</h3>
+            </div>
+            <div className={props.gameClass}>
+                <h3 onClick={props.gameClick}>Game data</h3>
+            </div>
+            <div className={props.importClass}>
+                <h3 onClick={props.importClick}>Importing data</h3>
+            </div>
+            <div className={props.migratingClass}>
+                <h3 onClick={props.migratingClick}>Migrating from IMDb</h3>
+            </div>
+            <div className={props.gameboxdClass}>
+                <h3 onClick={props.gameboxdClick}>Gameboxd brand</h3>
+            </div>
+            <div className={props.behindClass}>
+                <h3 onClick={props.behindClick}>Behind the scenes</h3>
+            </div>
+        </div>
+    )
 }
+
+
+export default AboutTabs
--- gameboy-box/src/Components/Routes/index.js
+++ gameboy-box/src/Components/Routes/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Landing from '../Landing'
 import GameDetails from '../GameDetails'
+import About from '../PageComponents/About'
 
 
 
@@ -10,6 +11,7 @@ const Routes = () => {
         <Switch>
             <Route exact path='/' component={Landing}/>
             <Route exact path='/games/:id' render={props => <GameDetails {...props}/>}/>
+            <Route exact path='/about' component={About}/>
         </Switch>
     )
 }
